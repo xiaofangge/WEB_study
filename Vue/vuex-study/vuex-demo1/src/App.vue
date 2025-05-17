42037ba5076ec1ec25b82de1f29aefa1e10c8d72
xiaofangge <1878364351@qq.com>
2025-05-18 00:41:11 +0800

    111

---
+++ Vue/vuex-study/vuex-demo1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Add></Add>
+    <hr />
+    <Sub></Sub>
+  </div>
+</template>
+
+<script>
+import Add from './components/Add.vue'
+import Sub from './components/Sub.vue'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    Add,
+    Sub,
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ Vue/vuex-study/vuex-demo1/src/components/Add.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3>当前最新的值为：</h3>
+    <button>+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
