99515a6da6720572511b68138916e9f12487bfe0
xiaofangge <1878364351@qq.com>
2025-05-24 12:06:49 +0800

    ref&this.$refs 学习

--- Vue/vue2_sync/src/App.vue
+++ Vue/vue2_sync/src/App.vue
@@ -2,17 +2,22 @@
   <div id="app">
     <button @click="openDialog">打开弹框</button>
     <BaseDialog :isShow.sync="isShow" />
+    <hr>
+    <BaseForm ref="baseform" />
+    <button @click="handleGet">获取数据</button>
+    <button @click="handleReset">重置数据</button>
   </div>
 </template>
 
 <script>
 
 import BaseDialog from './components/BaseDialog.vue';
+import BaseForm from './components/BaseForm.vue';
 
 export default {
   name: 'App',
   components: {
-    BaseDialog
+    BaseDialog, BaseForm
   },
   data() {
     return {
@@ -22,11 +27,17 @@ export default {
   methods: {
     openDialog() {
       this.isShow = true;
+    },
+    handleGet() {
+      console.log(this.$refs.baseform)
+      console.log(this.$refs.baseform.getValues())
+    },
+    handleReset() {
+      this.$refs.baseform.reset()
     }
   }
 }
 </script>
-
 <style scoped lang="css">
 .app {
   text-align: center;
---
+++ Vue/vue2_sync/src/components/BaseForm.vue
@@ -0,0 +1,48 @@
+<!--
+ * @author: Fang Ruichuan
+ * @since: 2025-05-24
+ * BaseForm.vue
+-->
+<template>
+    <div class="container">
+      <form action="">
+        <label for="account">账号</label>
+        <input type="text" id="account" v-model="account">
+        <label for="pwd">密码</label>
+        <input type="password" id="pwd" v-model="pwd">
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "BaseForm",
+    data() {
+      return {
+        account: '',
+        pwd: ''
+      }
+    },
+    methods: {
+      getValues() {
+        return {
+          account: this.account,
+          pwd: this.pwd
+        }
+      },
+      reset() {
+        this.account = ''
+        this.pwd = ''
+      }
+    }
+  }
+  </script>
+  
+  <style scoped lang="css">
+  .container {
+    padding: 10px 20px;
+    border: 1px solid black;
+    border-radius: 10px;
+  }
+  </style>
+  
