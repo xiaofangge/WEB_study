79e613825c06d9061fdbe550ca51bb38c146fd35
xiaofangge <1878364351@qq.com>
2025-05-24 11:27:58 +0800

    Vue2.sync 学习

---
+++ Vue/vue2_sync/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <button @click="openDialog">打开弹框</button>
+    <BaseDialog :isShow.sync="isShow" />
+  </div>
+</template>
+
+<script>
+
+import BaseDialog from './components/BaseDialog.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BaseDialog
+  },
+  data() {
+    return {
+      isShow: false
+    }
+  },
+  methods: {
+    openDialog() {
+      this.isShow = true;
+    }
+  }
+}
+</script>
+
+<style scoped lang="css">
+.app {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
---
+++ Vue/vue2_sync/src/components/BaseDialog.vue
@@ -0,0 +1,49 @@
+<!--
+ * @author: Fang Ruichuan
+ * @since: 2025-05-24
+ * BaseDialog.vue
+-->
+<template>
+  <div v-show="isShow" class="dialog">
+    <div class="dialog-content">
+        <h2>这是一个弹框</h2>
+        <button @click="closeDialog">关闭</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isShow: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        closeDialog() {
+            this.$emit('update:isShow', false)
+        }
+    }
+}
+</script>
+
+<style scoped lang="css">
+.dialog {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   
+}
+.dialog-content {
+    background-color: white;
+    padding: 20px;
+    border-radius: 5px;
+}
+</style>
